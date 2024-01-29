12d6a4f9fd81a9ea1cecca7fc7bc04cb3ffcd923
janvipravinbhaipatel <janvip119@gmail.com>
2024-01-29 12:41:46 -0500

    I have added some function calls to AddressBook.java to test Git Commit.

---
+++ firstlab_1/firstlab_1/src/main/java/BuddyInfo.java
@@ -0,0 +1,17 @@
+public class BuddyInfo {
+    private String name;
+    private String phoneNumber;
+
+    public BuddyInfo(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
